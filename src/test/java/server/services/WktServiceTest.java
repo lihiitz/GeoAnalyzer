10b633a320eb967de911c609d2e7a56c42cc9779
lihi <lihi@biot-med.com>
2023-03-06 08:19:26 +0200

    transferred tests to test folder

--- src/test/java/server/services/WktServiceTest.java
+++ src/test/java/server/services/WktServiceTest.java
@@ -1,13 +1,13 @@
-package server.test;
+package server.services;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import server.services.WktService;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-public class WktTests {
+class WktServiceTest {
 
     @Autowired
     private WktService service;
